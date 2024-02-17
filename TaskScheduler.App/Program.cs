fcdb2e1b04f184c9b47ff863099893f2e9fff5c2
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2024-02-17 23:37:42 +0100

    initial commit

---
+++ TaskScheduler.App/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace TaskScheduler.App
+{
+
+    // TASK:
+    // Change the Scheduler class such that it will ensure that SchedulerTask.Calculate() is executed regularly for each SchedulerTask
+    // with an interval that is as close as possible to the ScheulderTask.Interval.
+    //
+    // IMPORTANT: The accuracy of actual execution intervals should degrade gracefully when a *large number of tasks* is specified.
+    // Try to avoid 100% total CPU utilization (i.e. not all CPU cores should be at 100% all the time).
+    //
+    // you can assume:
+    //  - 8 CPU cores are available
+    //  - SchedulerTask.Calculate() takes up near 100% CPU utilization of a single core for the duration of the function call
+
+    public sealed class SchedulerTask // this class should not be changed
+    {
+        public TimeSpan Interval { get; }
+        public int TaskId { get; }
+
+        private static int nextId = 0;
+
+        public SchedulerTask()
+        {
+            this.Interval = TimeSpan.FromSeconds(Random.Shared.Next(1, 10)); // run every 1 to 10 seconds
+            this.TaskId = nextId++;
+        }
+
+        public void Calculate()
+        {
+            var startTime = DateTime.UtcNow;
+            Console.WriteLine($"{startTime.ToLongTimeString()}.{startTime.Millisecond} - ID: {TaskId} - Interval: {Interval.TotalSeconds}");
+
+            var endTime = startTime + TimeSpan.FromMilliseconds(Random.Shared.Next(500, 1500)); // run for at least 0.5, max 1.5 seconds
+            while (DateTime.UtcNow < endTime) ; // keep busy
+        }
+
+        static public IEnumerable<SchedulerTask> GetTasks()
+        {
+            while (true)
+            {
+                yield return new SchedulerTask();
+            }
+        }
+    }
+
+    public class Scheduler
+    {
+        private List<SchedulerTask> tasks;
+        public Scheduler(List<SchedulerTask> tasks)
+        {
+            this.tasks = tasks;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // add/modify code here to test with sample data (optional)
+            var tasks = SchedulerTask.GetTasks().Take(100).ToList();
+            var scheduler = new Scheduler(tasks);
+        }
+    }
+}
